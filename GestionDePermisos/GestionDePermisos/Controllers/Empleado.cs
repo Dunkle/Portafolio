5ce53e36f3299ba6ee18115c260c6e3087c608a5
Leonardo <leonardo.godoy.campos@gmail.com>
2018-10-06 20:10:19 -0300

    Clase motivos y negocio motivo.

--- GestionDePermisos/GestionDePermisos/Controllers/Empleado.cs
+++ GestionDePermisos/GestionDePermisos/Controllers/Empleado.cs
@@ -7,5 +7,23 @@ namespace GestionDePermisos.Controllers
 {
     public class Empleado
     {
+        int rut { get; set; }
+        string nombres {get;set;}
+        string apellidoPaterno { get; set; }
+        string apellidoMaterno { get; set; }
+        DateTime fechaNacimiento { get; set; }
+        string direccion { get; set; }
+
+        int telefono { get; set; }
+        string correo { get; set; }
+        DateTime fechaContratacion { get; set; }
+        int idEstado { get; set; }
+        int idCargo { get; set; }
+        int idComuna { get; set; }
+        int idDepartamento { get; set; }
+        int idCuenta { get; set; }
+        int idTipoContrato { get; set; }
+
+
     }
 }
