ad7a99ce35cb6f33fb26ff28c5259cefb91277b9
Sebastian Ignacio Salinas Salinas <sebastian.salinas@egt.cl>
2018-10-06 12:52:56 -0300

    se realizan cambios de css a permisos.css

---
+++ GestionDePermisos/GestionDePermisos/Controllers/Solicitud.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionDePermisos.Controllers
+{
+    public class Solicitud
+    {
+        public int idSolicitud { set; get; }
+        public String descripcion { set; get; }
+        public DateTime fechaSolicitud { set; get; }
+        public DateTime fechaInicio { set; get; }
+        public DateTime fechaTermino { set; get; }
+        public int idTipoPermiso { set; get; }
+        public int rutSolicitante { set; get; }
+        public int rutAutorizante { set; get; }
+        public int idEstado { set; get; }
+
+        public Solicitud()
+        {
+            idSolicitud = default(int);
+            descripcion = string.Empty;
+            //fechaInicio = DateTime.
+        }
+    }
+}
