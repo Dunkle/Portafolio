694e22fa2e10e27926687a5a042f77e00291cac6
Sebastian Ignacio Salinas Salinas <sebastian.salinas@egt.cl>
2018-10-06 16:02:46 -0300

    Se carga readall para solicitudes

--- GestionDePermisos/GestionDePermisos/Controllers/Solicitud.cs
+++ GestionDePermisos/GestionDePermisos/Controllers/Solicitud.cs
@@ -16,6 +16,8 @@ namespace GestionDePermisos.Controllers
         public int rutSolicitante { set; get; }
         public int rutAutorizante { set; get; }
         public int idEstado { set; get; }
+        public IList<Solicitud> coleccionSolicitudes { get; set; }
+
 
         public Solicitud()
         {
@@ -30,6 +32,32 @@ namespace GestionDePermisos.Controllers
             idEstado = default(int);
         }
 
+        public bool readAll()
+        {
+            try
+            {
+                this.coleccionSolicitudes = new List<Solicitud>();
+                foreach (var tmp in Conexion.entities.SOLICITUD.ToList())
+                {
+                    Solicitud solicitud = new Solicitud();
+                    solicitud.idSolicitud = Convert.ToInt32(tmp.IDSOLICITUD);
+                    solicitud.descripcion = tmp.DESCRIPCION;
+                    solicitud.fechaSolicitud = Convert.ToDateTime(tmp.FECHASOLICITUD);
+                    solicitud.fechaInicio = Convert.ToDateTime(tmp.FECHAINICIO);
+                    solicitud.fechaTermino = Convert.ToDateTime(tmp.FECHATERMINO);
+                    solicitud.idTipoPermiso = Convert.ToInt32(tmp.IDTIPOPERMISO);
+                    solicitud.rutSolicitante = Convert.ToInt32(tmp.RUT);
+                    solicitud.rutAutorizante = Convert.ToInt32(tmp.AUTORIZADOR);
+                    solicitud.idEstado = Convert.ToInt32(tmp.ESTADO);
 
+                    this.coleccionSolicitudes.Add(solicitud);
+                }
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
     }
 }
