6fb3a89db83850959b92ef78a98e6c14056872b2
Sebastian Ignacio Salinas Salinas <sebastian.salinas@egt.cl>
2018-11-20 02:31:50 -0300

    Se genera descarga de XML momentaneo

--- GestionDePermisos/GestionDePermisos/Views/Administrador/DescargaXMLAdmin.aspx.cs
+++ GestionDePermisos/GestionDePermisos/Views/Administrador/DescargaXMLAdmin.aspx.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using GestionDePermisos.Business;
+using GestionDePermisos.Controllers;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Web;
 using System.Web.Security;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Xml.Linq;
 
 namespace GestionDePermisos.Views.Administrador
 {
@@ -24,7 +25,52 @@ namespace GestionDePermisos.Views.Administrador
 
         protected void btnGenerarXML_Click(object sender, EventArgs e)
         {
+            NegocioSolicitud negocioSolicitud = new NegocioSolicitud();
 
+            XElement root = new XElement("Permiso");
+
+
+            var filename = "Permisos.xml";
+            var currentDirectory = @"C:\Users\Sebastian Salinas\Documents";
+            //File.Create(currentDirectory + filename);
+            var purchaseOrderFilepath = Path.Combine(currentDirectory, filename);
+
+            XElement collection = new XElement(root);
+            foreach (var item in negocioSolicitud.listado())
+            {
+                var parts = item.rutSolicitante.Split('-');
+                
+                var dias = (item.fechaTermino - item.fechaInicio).Days;
+                XElement permiso =
+                    new XElement("Permiso",
+                        new XElement("Funcionario",
+                            new XElement("Nombre", retornarNombreByRut(item.rutSolicitante)),
+                            new XElement("Run", parts[0]),
+                            new XElement("Dvrun", parts[1])
+                            ),
+                        new XElement("Detalle",
+                            new XElement("Fecha_inicio", item.fechaInicio),
+                            new XElement("Fecha_fin", item.fechaTermino),
+                            new XElement("Fecha_fin", dias),
+                            new XElement("Fecha_fin", retornaNombreMotivo(item.idTipoPermiso))
+                        )
+                    );
+
+                collection.Add(permiso);
+                
+            }
+            collection.Add();
+            collection.Save(purchaseOrderFilepath);
+        }
+        private string retornarNombreByRut(string rut)
+        {
+            NegocioEmpleado negocioEmpleado = new NegocioEmpleado();
+            return negocioEmpleado.retornarNombreByRut(rut);
+        }
+        private string retornaNombreMotivo(int id)
+        {
+            NegocioMotivo negocioMotivo = new NegocioMotivo();
+            return negocioMotivo.nameByID(id);
         }
     }
 }
