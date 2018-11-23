c2603318a20fab2203bf55e9587aa6c30fe8649b
Leonardo godoy campos <leo_thecure@hotmail.com>
2018-11-23 18:13:50 -0300

    WS Deserializado

--- GestionDePermisos/GestionDePermisos/Business/ConsumirWS.cs
+++ GestionDePermisos/GestionDePermisos/Business/ConsumirWS.cs
@@ -5,41 +5,27 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Web;
+using GestionDePermisos.Models;
+using Newtonsoft.Json;
 
 namespace GestionDePermisos.Business
 {
     public class ConsumirWS
     {
-        public void ConsultaAntiguedad(string rut) {
-            //WebClient webClient = new WebClient();
-            //string url = string.Format("http://localhost:8585/Sistema-rrhh-1.0.0/empleado/{0}",rut);
-            //byte[] datos = webClient.DownloadData(url);
-            //Stream memoria = new MemoryStream(datos);
-            //var reader = new StreamReader(memoria);
-            //var result = reader.ReadToEnd();
-
-            //byte[] data = UTF8Encoding.UTF8.GetBytes(rut);
-            //HttpWebRequest request;
-            //request = WebRequest.Create("http://localhost:8585/Sistema-rrhh-1.0.0/empleado/") as HttpWebRequest;
-            //request.Timeout = 10 * 1000;
-            //request.Method = "POST";            
-            //request.ContentType = "application/json; charset=utf-8";
-            //Stream postStream = request.GetRequestStream();
-            //postStream.Write(data, 0, data.Length);
-
-            //HttpWebResponse response = request.GetResponse() as HttpWebResponse;
-            //StreamReader reader = new StreamReader(response.GetResponseStream());
-            //string body = reader.ReadToEnd();
-
+        public int ConsultaAntiguedad(string rut) {
+            string url = string.Format("http://localhost:8585/Sistema-rrhh-1.0.0/empleado/{0}",rut);
+            
             var syncClient = new WebClient();
             syncClient.Headers[HttpRequestHeader.ContentType] = "application/json; charset=utf-8";
             syncClient.Headers[HttpRequestHeader.Accept] = "application/json";
 
-            var resp = syncClient.UploadString("http://localhost:8585/Sistema-rrhh-1.0.0/empleado/", "POST", "{19}");
+            var resp = syncClient.UploadString(url, "POST");
 
-            string test = string.Empty;
+            AntiguedadEmpleado empleado;
 
+            empleado = JsonConvert.DeserializeObject<AntiguedadEmpleado>(resp);
 
+            return empleado.antiguedad;
         }
     }
 }
---
+++ GestionDePermisos/GestionDePermisos/Models/Antiguedad.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionDePermisos.Models
+{
+    public class AntiguedadEmpleado
+    {
+        public string rut { get; set; }
+        public int antiguedad { get; set; }
+    }
+}
